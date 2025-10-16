136eef2a41b5e0c7aa7bb0123f6a0667b029babf
Thomas Gallaher <thomassgallaher@gmail.com>
2025-10-16 12:04:48 -0400

    Initial project setup

---
+++ Lab5_CarIntro/Lab5_CarIntro.ino
@@ -0,0 +1,38 @@
+// Pins for all inputs, keep in mind the PWM defines must be on PWM pins
+#define MotorPWM_A 4 //left motor
+#define MotorPWM_B 5 //right motor
+
+#define INA1A 32
+#define INA2A 34
+#define INA1B 30
+#define INA2B 36
+
+void setup() {
+    pinMode(MotorPWM_A, OUTPUT);
+    pinMode(MotorPWM_B, OUTPUT);
+    pinMode(INA1A, OUTPUT);
+    pinMode(INA2A, OUTPUT);
+    pinMode(INA1B, OUTPUT);
+    pinMode(INA2B, OUTPUT);
+}
+ 
+// Method: Forward
+// Input: speed – value [0-255]
+// Rotate the motor in a clockwise fashion
+void Forward(int speed)
+{
+  analogWrite(MotorPWM_A, speed);
+  analogWrite(MotorPWM_B, speed);
+
+  // Left Motor
+  digitalWrite(INA1A, HIGH);
+  digitalWrite(INA2A, LOW);
+
+  // Right Motor
+  digitalWrite(INA1B, HIGH);
+  digitalWrite(INA2B, LOW);
+}
+
+void loop() {
+  Forward(100);
+}
